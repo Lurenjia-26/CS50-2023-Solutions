50de7d16ba99e1b2900b6199e9ac4ed7e54a0a95
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-16 11:55:59 +0000

    Fri, Dec 16, 2022, 11:55 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -12,37 +12,33 @@ int main(void)
     do
     {
         num = get_long("Number: ");
-    } while(num > 0);
+    } while (num > 0 && num < 1e17);
 
-    // 如果位数合格，则进行更精确的号码真伪识别
-    if (digits == 13 || digits == 15 || digits == 16)
+    // 将每一位数储存在数组中
+    for (digits = 0, tmp_num = num; tmp_num > 1; digits++)
     {
-        // 将每一位数储存在数组中
-        for (digits = 0, tmp_num = num; tmp_num > 1; digits++)
+        a[digits] = tmp_num % 10;
+        tmp_num /= 10;
+    }
+    // 隔一位乘2并求和
+    for (int i = 1; i < digits; i += 2)
+    {
+        if (a[i] < 5)
         {
-            a[digits] = tmp_num % 10;
-            tmp_num /= 10;
+            sum += a[i] * 2;
         }
-        // 隔一位乘2并求和
-        for (int i = 1; i < digits; i += 2)
+        else
         {
-            if (a[i] < 5)
-            {
-                sum += a[i] * 2;
-            }
-            else
-            {
-                sum += a[i] / 5 + (a[i] * 2) % 10;
-            }
+            sum += a[i] / 5 + (a[i] * 2) % 10;
         }
-        // 与未乘的数加起来
-        for (int i = 0; i < digits; i += 2)
-        {
-            sum += a[i];
-        }
-        if (sum % 10 == 0)
-        {
-            isValid = true;
-        }
+    }
+    // 与未乘的数加起来
+    for (int i = 0; i < digits; i += 2)
+    {
+        sum += a[i];
+    }
+    if (sum % 10 == 0)
+    {
+        isValid = true;
     }
 }

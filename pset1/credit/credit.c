3e3012bf62382d8be444d4b430b4ab39dbc2a71b
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-16 10:55:48 +0000

    Fri, Dec 16, 2022, 10:55 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -7,19 +7,23 @@ int main(void)
     int digits = 1;
     bool isValid = false;
 
-    num = get_long("Number: ");
-    //算出位数
-    for (long tmp_num = num; tmp_num > 10; digits++)
+    do
     {
+        num = get_long("Number: ");
+        //算出位数
+        for (long tmp_num = num; tmp_num > 10; digits++)
+        {
         tmp_num /= 10;
-    }
-    if (digits == 13 || digits == 15 || digits == 16)
-    {
-        //进行更精确的号码真伪识别
+        }
+        if (digits == 13 || digits == 15 || digits == 16)
+        {
+            //进行更精确的号码真伪识别
 
+        }
+        if (!isValid)
+        {
+            printf("foo\n");
+        }
     }
-    if (!isValid)
-    {
-        printf("foo\n");
-    }
+    while (!isValid)
 }

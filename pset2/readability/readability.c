22c8495e1a13d81451f69f44034d491a2bf91556
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-18 07:17:14 +0000

    Sun, Dec 18, 2022, 7:17 AM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -2,35 +2,37 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <math.h>
 
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
-float cal_index(float l, float s);
+float cal_index(int letters, int words, int sentences);
 
 int main(void)
 {
     int letters, words, sentences;
-    float l, s, index;
+    float index;
     string text = get_string("Text: ");
 
     letters = count_letters(text);
     words = count_words(text);
     sentences = count_sentences(text);
-    l = 
-    index = cal_index(l, s);
+    index = cal_index(letters, words, sentences);
 
-    printf("Grade %.0f\n", index);
+    printf("Grade %.2f\n", index);
 }
 
 int count_letters(string text)
 {
     int count = 0;
+
     for (int i = 0; i < strlen(text); i++)
     {
         if (isalnum(text[i]))
             count++;
     }
+
     return count;
 }
 
@@ -45,21 +47,27 @@ int count_words(string text)
             count++;
         pre_letter = text[i];
     }
+
     return count;
 }
 
 int count_sentences(string text)
 {
     int count = 0;
+
     for (int i = 0; i < strlen(text); i++)
     {
         if (ispunct(text[i]) && (text[i] == '.' || text[i] == '!' || text[i] == '?'))
             count++;
     }
+
     return count;
 }
 
-float cal_index(float l, float s)
+float cal_index(int letters, int words, int sentences)
 {
-    return (0.0588 * l - 0.296 * s - 15.8)
+    float l = letters / words * 100;
+    float s = sentences / words * 100;
+
+    return (0.0588 * l - 0.296 * s - 15.8);
 }

4974f0f45de440c2d0614c6f7131f71bef380493
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-18 06:15:26 +0000

    Sun, Dec 18, 2022, 6:15 AM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -3,9 +3,30 @@
 #include <ctype.h>
 
 int count_letters(string text);
+int count_words(string text);
 int count_sentences(string text);
 
 int main(void)
+{
+    int text = get_string("Text: ");
+    int letters, words, sentences;
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if()
+    }
+}
+
+int count_words(string text);
+{
+
+}
+
+int count_sentences(string text)
 {
 
 }

4613aba838bc0a22ba2819eaab83a1ab1c3702c0
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-18 06:22:29 +0000

    Sun, Dec 18, 2022, 6:22 AM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -9,8 +9,11 @@ int count_sentences(string text);
 
 int main(void)
 {
-    int text = get_string("Text: ");
     int letters, words, sentences;
+    string text = get_string("Text: ");
+
+    letters = count_letters(text);
+    printf("%d letters\n", letters);
 }
 
 int count_letters(string text)
@@ -18,17 +21,22 @@ int count_letters(string text)
     int count = 0;
     for (int i = 0; i < strlen(text); i++)
     {
-        if(isletter(text[i]))
+        if(isalnum(text[i]))
             count++;
     }
+    return count;
 }
 
-int count_words(string text);
+int count_words(string text)
 {
+    int count = 0;
 
+    return count;
 }
 
 int count_sentences(string text)
 {
+    int count = 0;
 
+    return count;
 }

2caf216691a3d0ed378db16ce491362bf5a49666
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-19 13:41:43 +0000

    Mon, Dec 19, 2022, 1:41 PM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -1,9 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     string key = argv[1];
     string s = get_string("plaintext: ");
-    
+
+    for (int i = 0, n = strlen(s); i < n)
+    {
+        if ()
+    }
 }

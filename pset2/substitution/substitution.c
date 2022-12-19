19fdfbf44c4a893db9841f9a792fed8ae4fdc717
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-19 13:47:33 +0000

    Mon, Dec 19, 2022, 1:47 PM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -5,11 +5,31 @@
 
 int main(int argc, string argv[])
 {
-    string key = argv[1];
-    string s = get_string("plaintext: ");
-
-    for (int i = 0, n = strlen(s); i < n)
+    string key = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    if (strlen(key) != 26)
     {
-        if ()
+        
+    }
+    string s = get_string("plaintext:  ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (isalpha(s[i]))
+        {
+            if (isupper(s[i]))
+            {
+                s[i] = key[s[i] - 'a'];
+            }
+            else
+            {
+                s[i] = key[s[i] - 'A'];
+            }
+        }
+    }
+
+    printf("ciphertext: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
     }
 }

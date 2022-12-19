097ab954624a08f94e640806d5d1511b9b60a812
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2022-12-19 13:52:37 +0000

    Mon, Dec 19, 2022, 1:52 PM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -6,14 +6,18 @@
 int main(int argc, string argv[])
 {
     string key = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
+        return 1;
     }
     if (strlen(key) != 26)
     {
         printf("Key must contain 26 characters.\n");
+        return 1;
     }
+    
     string s = get_string("plaintext:  ");
 
     for (int i = 0, n = strlen(s); i < n; i++)

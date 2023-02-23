f6cc28c0d30acf59647523db86d8f4d82f16e440
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-02-23 05:59:06 +0000

    Thu, Feb 23, 2023, 1:59 PM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -219,13 +219,16 @@ void print_winner(void)
 {
     // TODO
     int winner = 0;
-    for (int i = 0; i < pair_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        for
-        if (locked[i][winner])
+        for (int j = 0; j < candidate_count; j++)
         {
-            winner = i;
+            if (locked[j][winner])
+            {
+                winner = j;
+            }
         }
+
     }
     return;
 }

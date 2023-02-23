5edcab1980bf5912596dc5d6f1ee087e9092c8c1
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-02-23 03:32:28 +0000

    Thu, Feb 23, 2023, 11:32 AM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -131,6 +131,16 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count)
+    {
+        for (int j = i + 1; j < candidate_count)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[i]
+            }
+        }
+    }
     return;
 }
 

8d2fb222a7db07a8d973275068f62d162eef96f1
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-02-23 02:30:39 +0000

    Thu, Feb 23, 2023, 10:30 AM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -100,7 +100,7 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
-    for (int i = 0; i < candidate_count)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i]) == 0)
         {

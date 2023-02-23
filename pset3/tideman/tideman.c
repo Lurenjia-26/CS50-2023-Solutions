811ec54c43ee61eb1a730d6d86302d9ea7c1effa
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-02-23 03:36:18 +0000

    Thu, Feb 23, 2023, 11:36 AM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -139,12 +139,13 @@ void add_pairs(void)
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
+                pair_count++;
             }
             else if (preferences[i][j] < preferences[j][i])
             {
                 pairs[pair_count].winner = j;
                 pairs[pair_count].loser = i;
-                
+                pair_count++;
             }
         }
     }

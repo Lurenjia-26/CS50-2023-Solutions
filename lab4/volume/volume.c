ace8b170984f5b81fd2a74643531dc95873c1f64
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-11 08:11:14 +0000

    Sat, Mar 11, 2023, 4:11 PM +08:00

--- lab4/volume/volume.c
+++ lab4/volume/volume.c
@@ -35,9 +35,13 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header;
-    fread()
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEAD_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    uint16_t sample;
+    for (int i = 0; i < )
+    fread()
 
     // Close files
     fclose(input);

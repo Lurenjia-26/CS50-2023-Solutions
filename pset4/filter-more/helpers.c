a43656d53b0e90fc282aabc08336fa5c52784e5c
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-11 10:03:11 +0000

    Sat, Mar 11, 2023, 6:03 PM +08:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -23,19 +23,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     RGBTRIPLE temp;
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width / 2; j++)
         {
-            temp.rgbtRed = image[i][j].rgbtRed;
-            temp.rgbtGreen = image[i][j].rgbtGreen;
-            temp.rgbtBlue = image[i][j].rgbtBlue;
-
-            image[i][j].rgbtRed = image[i][width - 1 - j].rgbtRed;
-            image[i][j].rgbtGreen = image[i][width - 1 - j].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][width - 1 - j].rgbtBlue;
-
-            image[i][width - 1 - j].rgbtRed = temp.rgbtRed;
-            image[i][width - 1 - j].rgbtGreen = temp.rgbtGreen;
-            image[i][width - 1 - j].rgbtBlue = temp.rgbtBlue;
+            temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
         }
     }
     return;

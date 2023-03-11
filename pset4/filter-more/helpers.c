936ed1bb0ee7c51a228a3ef94014f36bb9d2fe85
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-11 09:44:14 +0000

    Sat, Mar 11, 2023, 5:44 PM +08:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -1,9 +1,19 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avgrgb = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = avgrgb;
+            image[i][j].rgbtGreen = avgrgb;
+            image[i][j].rgbtBlue = avgrgb;
+        }
+    }
     return;
 }
 

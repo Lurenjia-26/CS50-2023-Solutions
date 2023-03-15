d3e87f593e028f8105b2f1e63b39ce814d952582
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-15 04:33:05 +0000

    Wed, Mar 15, 2023, 12:33 PM +08:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -126,7 +126,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     {
                         continue;
                     }
-                    
+                    red_x = Gx[]
                 }
             }
         }

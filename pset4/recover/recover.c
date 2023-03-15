509c66fe6a693f8e2b91aafd4c1342f9bc200ba5
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-15 08:13:41 +0000

    Wed, Mar 15, 2023, 4:13 PM +08:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define BLOCK_SIZE 512
 
+bool isJPEG(uint8_t *buffer);
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -24,4 +26,13 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+bool isJPEG(uint8_t *buffer)
+{
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] == )
+    {
+        return true;
+    }
+    return false;
 }

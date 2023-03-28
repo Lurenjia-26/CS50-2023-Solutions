d9938bbfd5e421fea026339f3f1805707df1a0b9
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-28 13:23:13 +0000

    Tue, Mar 28, 2023, 9:23 PM +08:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -2,24 +2,22 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "dictionary.h"
 
 // Represents a node in a hash table
 typedef struct node
 {
-    bool end_word;
-    struct node *next[27];
+    char word[LENGTH + 1];
+    struct node *next;
 }
 node;
 
-//Roots of Tries
-node *root_s, *root_l;
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
 
-//Size of Trie
-unsigned dict_size = 0;
+// Hash table
+node *table[N];
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -29,53 +27,16 @@ bool check(const char *word)
 }
 
 // Hashes word to a number
-unsigned int hash(char word)
+unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    if (word == '\'')
-    {
-
-    }
-    return 26;
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    FILE *dict_s = fopen("dictionaries/large", "r");
-    FILE *dict_l = fopen("dictionaries/small", "r");
-    if (dict_l == NULL || dict_s == NULL)
-    {
-        return false;
-    }
-    root_s = malloc(sizeof(node));
-    root_l = malloc(sizeof(node));
-    node *cur_s = root_s;
-    node *cur_l = root_l;
-
-    char ch;
-    while ((ch = fgetc(dict_s)) != EOF)
-    {
-        if (ch == '\n')
-        {
-            cur_s->end_word = true;
-            cur_s = root_s;
-            dict_size++;
-        }
-        else
-        {
-            unsigned i = hash(ch);
-            if (cur_s->next[i] == NULL)
-            {
-                cur_s->next[i] = malloc(sizeof(node));
-            }
-            cur_s = cur_s->next[i];
-        }
-    }
-
-    fclose(dict_s);
-    fclose(dict_l);
     return false;
 }
 

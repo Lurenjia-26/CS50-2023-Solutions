a6f4281c9d53775d8e3beab3ad971494bdde2840
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-28 13:51:22 +0000

    Tue, Mar 28, 2023, 9:51 PM +08:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -55,7 +55,8 @@ bool load(const char *dictionary)
         return false;
     }
 
-    while (fscanf())
+    char word[LENGTH + 1];
+    while ((word = fscanf()))
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded

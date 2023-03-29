3f1ffd16736702b5b1eec0d9c001e66e5e10ea16
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-29 10:10:52 +0000

    Wed, Mar 29, 2023, 6:10 PM +08:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -49,19 +49,18 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    // unsigned int hash_value = 0;
-    // int len = strlen(word);
+    unsigned int hash_value = 0;
+    int len = strlen(word);
 
-    // for (int i = 0; i < len; i++)
-    // {
-    //     if (isalpha(word[i]))
-    //     {
-    //         hash_value += toupper(word[i]) - 'A';
-    //     }
-    // }
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(word[i]))
+        {
+            hash_value += toupper(word[i]) - 'A';
+        }
+    }
 
-    // return hash_value;
-    return toupper(word[0]) - 'A';
+    return hash_value;
 }
 
 // Loads dictionary into memory, returning true if successful, else false

1f4b2a6d8fa192e865f8bfed60dbf8d29eb2adb0
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-29 02:51:19 +0000

    Wed, Mar 29, 2023, 10:51 AM +08:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -20,7 +20,7 @@ node;
 void free_node(node *cursor);
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 60;
 
 // Hash table
 node *table[N];
@@ -53,14 +53,14 @@ unsigned int hash(const char *word)
 
     if (strlen(word) > 1)
     {
-        hash_value = (tolower(word[0]) - 'a' + 1) * (tolower(word[1]) - 'a' + 1);
+        hash_value = (tolower(word[0]) - 'a') + (tolower(word[1]) - 'a');
     }
     else
     {
-        hash_value = (tolower(word[0]) - 'a' + 1) * 26;
+        hash_value = (tolower(word[0]) - 'a') * 2;
     }
 
-    return hash_value % N;
+    return hash_value;
 }
 
 // Loads dictionary into memory, returning true if successful, else false

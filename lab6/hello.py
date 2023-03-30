a1c2cc9fafc0f2b732822541d86d877035d7a206
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-30 07:43:34 +0000

    Thu, Mar 30, 2023, 3:43 PM +08:00

--- lab6/hello.py
+++
@@ -1,7 +0,0 @@
-import get_int from cs50
-import get_string from cs50
-
-x = get_int("Please input first number")
-y = get_int("Please input second number")
-
-print(f"x + y = {x + y}\nx * y = {x * y}")

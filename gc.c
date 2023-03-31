b6dd80ab9f484209d43b2f61d84560b51e4b4ce6
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-31 06:03:36 +0000

    Fri, Mar 31, 2023, 2:03 PM +08:00

--- gc.c
+++ gc.c
@@ -1,15 +1,43 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 bool isPrime(int n);
 
 int main(void)
 {
 
+    int l = get_int("Length: ");
+    for (int i = 0; i < l; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d\n", i);
+        }
+    }
     return 0;
 }
 
 bool isPrime(int n)
 {
-    if (n < 3)
+    if (n <= 3)
+    {
+        return (n > 1);
+    }
+    if (n % 6 != 1 && n % 6 != 5)
+    {
+        return false;
+    }
+
+    int sq = (int)sqrt(n);
+    for (int i = 5; i < sq; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }

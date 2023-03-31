4a9b049b11d648adebf7951d797b9455d11d3aa2
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-31 06:53:51 +0000

    Fri, Mar 31, 2023, 2:53 PM +08:00

--- gc.c
+++ gc.c
@@ -2,45 +2,21 @@
 #include <stdbool.h>
 #include <math.h>
 
-bool isPrime(int n)
+void f(int n)
 {
-    if (n <= 3)
-    {
-        return (n > 1);
-    }
-    if (n % 6 != 1 && n % 6 != 5)
-    {
-        return false;
-    }
+    for (int i = 1; i <= n)
+}
 
-    int sq = (int)sqrt(n);
-    for (int i = 5; i <= sq; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return false;
-        }
-    }
+void sf(int n)
+{
 
-    return true;
 }
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
-    for (int i = 2; i <= n; i += 2)
-    {
-        for (int j = 2; j < i; j++)
-        {
-            int k = i - j;
-            if (isPrime(j) && isPrime(k))
-            {
-                printf("%d=%d+%d\n", i, j, k);
-                break;
-            }
-        }
-    }
+    sf(n);
 
     return 0;
 }

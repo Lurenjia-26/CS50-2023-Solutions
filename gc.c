a5da08cc62dd2f97d6a18e1e2adff9b239886fa3
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-31 07:32:39 +0000

    Fri, Mar 31, 2023, 3:32 PM +08:00

--- gc.c
+++ gc.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
-void f(int *sum, int *a, int *b, int n)
-{
-    for (int i = 0; i < 60; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-
-        }
-    }
-
-    return;
-}
+int s[60] = {-1};
+int a[60] = {0};
 
 void sf(int n)
 {
-    int sum[60] = {-1};
-    int a[60] = {0};
-    int b[60] = {0};
+
 
     for (int i = 1; i < 60; i++)
     {

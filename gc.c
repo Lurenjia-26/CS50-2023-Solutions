16204717343e6cd49511d4db62d7f37876804c0a
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-03-31 07:54:15 +0000

    Fri, Mar 31, 2023, 3:54 PM +08:00

--- gc.c
+++ gc.c
@@ -7,7 +7,7 @@ int a[60] = {0};
 void mul(int t)
 {
     int x = 0;
-    for (int i = 0; i < 60 i++)
+    for (int i = 0; i < 60; i++)
     {
         a[i] = a[i] * t + x;
         x = a[i] / 10;
@@ -29,15 +29,25 @@ void add()
 
 void output()
 {
-    
+    int i;
+    for (i = 59; i >= 0; i--)
+    {
+        if(s[i])
+            break;
+    }
+    for (int j = i; j >= 0; j--)
+    {
+        printf("%d", s[j]);
+    }
 }
 
 int main(void)
 {
     scanf("%d", &n);
-    for (int i = 2; i <= n; i++)
+    a[0] = 1;
+    for (int i = 1; i <= n; i++)
     {
-        mud(i);
+        mul(i);
         add();
     }
     output();

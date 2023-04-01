191cf49637d01d26a75103b728194c44cbe49c11
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-01 05:32:46 +0000

    Sat, Apr 1, 2023, 1:32 PM +08:00

--- lab6/mul.py
+++ lab6/mul.py
@@ -1,25 +1,7 @@
+import get_int() from cs50
+
+
 def main():
-    while True:
-        try:
-            n = input("Height: ")
-            n = int(n)
-            if n in range(1, 10):
-                break
-            elif n > 9:
-                print("Too much!")
-            elif n < 1:
-                print("Are you kidding?")
-        except ValueError:
-            print(f"{n} is not an integer")
-    mul(n)
-
-
-def mul(n):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if i >= j:
-                print(f"{j} * {i} = {i * j} ",end="")
-        print("")
-
-
-main()
+    n = get_int("Height: ")
+    for i in range(n):
+        print("#" * n)

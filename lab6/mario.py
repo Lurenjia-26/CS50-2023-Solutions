03da8a2a96669d25150b436ab387a2407c329565
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-01 05:00:11 +0000

    Sat, Apr 1, 2023, 1:00 PM +08:00

--- lab6/mario.py
+++ lab6/mario.py
@@ -3,10 +3,10 @@ def main():
     mario(n)
 
 def mario(n):
-    for i in range(n):
-        for j in range(n):
-            if i <= j:
-                print("#",end="")
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i >= j:
+                print(f"{j} * {i} = {i * j} ",end="")
         print("")
 
 main()

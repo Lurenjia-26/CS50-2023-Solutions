0d06b7e84c99ff9e32f4f6dda9fa49aff7d27c31
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-01 05:13:58 +0000

    Sat, Apr 1, 2023, 1:13 PM +08:00

--- lab6/mul.py
+++ lab6/mul.py
@@ -9,11 +9,13 @@ def main():
             print("Are you kidding?")
     mario(n)
 
+
 def mario(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if i >= j:
                 print(f"{j} * {i} = {i * j} ",end="")
         print("")
+
 
 main()

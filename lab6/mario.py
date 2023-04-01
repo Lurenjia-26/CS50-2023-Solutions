ac4e7946fec32030d6e800fcbfb0c00b5f782a00
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-01 04:50:28 +0000

    Sat, Apr 1, 2023, 12:50 PM +08:00

--- lab6/mario.py
+++ lab6/mario.py
@@ -1,9 +1,10 @@
 def main():
-    n = int(input)
+    n = int(input("Height: "))
     mario(n)
-
 
 def mario(n):
     for i in range(n):
         for j in range(n):
-            print("#")
+            print(f"#")
+
+main()

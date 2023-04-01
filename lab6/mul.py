a0420d7138cb0ffa66401c77ae63c4f9093f7e22
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-01 05:23:05 +0000

    Sat, Apr 1, 2023, 1:23 PM +08:00

--- lab6/mul.py
+++ lab6/mul.py
@@ -1,19 +1,20 @@
 def main():
     while True:
         try:
-            n = int(input("Height: "))
-            if n in range(1, 9):
+            n = input("Height: ")
+            n = int(n)
+            if n in range(1, 10):
                 break
             elif n > 9:
                 print("Too much!")
             elif n < 1:
                 print("Are you kidding?")
         except ValueError:
-            print("Not an integer")
-    mario(n)
+            print(f"{n} is not an integer")
+    mul(n)
 
 
-def mario(n):
+def mul(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if i >= j:

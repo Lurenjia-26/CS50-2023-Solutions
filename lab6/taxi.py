29d9dd75536b679ce2cc6e7d61850b02c5764b4e
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-02 14:17:34 +0000

    Sun, Apr 2, 2023, 10:17 PM +08:00

--- lab6/taxi.py
+++ lab6/taxi.py
@@ -19,17 +19,19 @@ def randLicense():
 
 
 def est(li):
-    li = list(li)
-    li.sort()
-
-    max0 = 2 * sum(li) / len(li)
-    max1 = li[int(LENGTH / 2)] + li[int(LENGTH / 2 - 1)]
-    max2 = li[LENGTH - 1] + li[0] - MIN
-    max3 = int((1 + 1 / (LENGTH)) * li[LENGTH - 1])
-
-    # print(li)
     print("Avg\t\tMedian\t\tGap\t\tAvg Gap")
-    print(f"{max0}\t\t{max1}\t\t{max2}\t\t{max3}\n")
+    for i in range(5):
+        li = list(li)
+        li.sort()
+
+        max0 = 2 * sum(li) / len(li)
+        max1 = li[int(LENGTH / 2)] + li[int(LENGTH / 2 - 1)]
+        max2 = li[LENGTH - 1] + li[0] - MIN
+        max3 = int((1 + 1 / (LENGTH)) * li[LENGTH - 1])
+
+        # print(li)
+        print(f"{max0}\t\t{max1}\t\t{max2}\t\t{max3}")
+
     print(f"Answer: {MAX}")
 
 

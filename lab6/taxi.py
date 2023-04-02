1c66c257a856f6002f5995a883481500b83febaa
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-02 13:59:32 +0000

    Sun, Apr 2, 2023, 9:59 PM +08:00

--- lab6/taxi.py
+++ lab6/taxi.py
@@ -1,5 +1,7 @@
 import random
 LENGTH = 10
+MIN = 0
+MAX = 1000
 
 
 def main():
@@ -10,7 +12,7 @@ def main():
 def randLicense():
     li = set()
     while True:
-        li.add(random.randint(0, 1000))
+        li.add(random.randint(MIN, MAX))
         if (len(li) == LENGTH):
             break
     return li
@@ -20,10 +22,12 @@ def est(li):
     li = list(li)
     li.sort()
     max0 = 2 * sum(li) / len(li)
-    max1 = max0
+    max1 = 2 * li[int(LENGTH / 2)]
+    max2 = li[LENGTH - 1] + (li[0] - MIN)
+    max3 = 0
     print(li)
-    print("Mean\t")
-    print(f"{max0}\n{max1}")
+    print("Avg\t Median\t    Gap\t  Avg Gap")
+    print(f"{max0}\t {max1}\t    {max2}\t  {max3}")
 
 
 main()

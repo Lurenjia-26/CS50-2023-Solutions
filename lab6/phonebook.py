bd11c85f571f3e9290a05fd1f4aa6efe8e684cc4
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-05 11:52:23 +0000

    Wed, Apr 5, 2023, 7:52 PM +08:00

---
+++ lab6/phonebook.py
@@ -0,0 +1,12 @@
+import csv
+
+with open("phonebook.csv", "a") as file:
+    while True:
+        name = input("Name: ")
+        number = input("Number: ")
+
+        if number == -1:
+            break
+
+        writer = csv.writer(file)
+        writer.writerow([name, f" {number}"])

51218ae1d98733dcb156222e9ce23a928a274eff
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-06 07:49:13 +0000

    Thu, Apr 6, 2023, 3:49 PM +08:00

--- python/mario.py
+++ python/mario.py
@@ -1,4 +1,8 @@
 h = int(input("Height: "))
 
 for i in range(h):
-    
+    for j in range(h):
+        if j < h - i - 1:
+            print(" ", end="")
+        else:
+            print("#", end="")

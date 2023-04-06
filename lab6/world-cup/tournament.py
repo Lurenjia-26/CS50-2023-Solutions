c9b47eeb468707df871631f7959124e55f9c25a7
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-06 08:12:11 +0000

    Thu, Apr 6, 2023, 4:12 PM +08:00

--- lab6/world-cup/tournament.py
+++ lab6/world-cup/tournament.py
@@ -19,7 +19,8 @@ def main():
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            row =
+            row["rating"] = int(row["rating"])
+            teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts

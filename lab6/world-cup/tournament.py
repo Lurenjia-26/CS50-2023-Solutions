55367a72cfa5b8ae3f6ed9a02361255fb12e9611
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-06 08:08:24 +0000

    Thu, Apr 6, 2023, 4:08 PM +08:00

--- lab6/world-cup/tournament.py
+++ lab6/world-cup/tournament.py
@@ -18,6 +18,8 @@ def main():
     # TODO: Read teams into memory from file
     with open("argv[1]") as file:
         reader = csv.DictReader(file)
+        for row in reader:
+            teams[row] = 
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts

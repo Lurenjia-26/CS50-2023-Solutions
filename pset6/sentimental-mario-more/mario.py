f5a365f82746ff4c4c08bcb192f8265fa419c82f
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-09 09:10:33 +0000

    Sun, Apr 9, 2023, 5:10 PM +08:00

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -1,12 +1,12 @@
 # TODO
 while True:
     h = int(input("Height: "))
-    if (h in range(1, 8)):
+    if (h in range(0, 9)):
         break
-    print("h should in [1, 8]")
+    print("h should in (1, 8)")
 
 for i in range(h):
     print(" " * (h - i - 1), end="")
     print("#" * (i + 1), end="")
-    print(" ", end="")
+    print("  ", end="")
     print("#" * (i + 1))

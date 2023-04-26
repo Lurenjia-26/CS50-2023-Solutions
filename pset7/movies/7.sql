1d3e91fcbff576fa604208a616adead62f624402
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-26 10:20:54 +0000

    Wed, Apr 26, 2023, 6:20 PM +08:00

--- pset7/movies/7.sql
+++ pset7/movies/7.sql
@@ -2,4 +2,4 @@ SELECT ratings.rating, movies.title
 FROM ratings
 INNER JOIN movies ON movies.id = ratings.movie_id
 WHERE movies.year = 2010
-ORDER BY ratings.rating DESC, movies.title DESC;
+ORDER BY ratings.rating DESC, movies.title ASC;

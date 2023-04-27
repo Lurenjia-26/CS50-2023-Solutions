08a1cb2b49423859a72daea28de92fb297a6f7da
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-27 05:59:08 +0000

    Thu, Apr 27, 2023, 1:59 PM +08:00

---
+++ pset7/movies/11.sql
@@ -0,0 +1,7 @@
+SELECT title
+FROM movies, stars, ratings, people
+WHERE stars.person_id = people.id
+AND stars.movie_id = movies.id
+AND people.name = "Chadwick Boseman"
+ORDER BY rating DESC
+LIMIT 10;

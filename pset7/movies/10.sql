97bc45bd8bb69163366a6d9ee2ca47f67f709b36
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-04-27 05:53:50 +0000

    Thu, Apr 27, 2023, 1:53 PM +08:00

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT(name)
-FROM 
-WHERE stars.person_id = people.id
+FROM directors, ratings, people
+WHERE 
 AND stars.movie_id = movies.id
 AND movies.year = 2004
 ORDER BY people.birth;

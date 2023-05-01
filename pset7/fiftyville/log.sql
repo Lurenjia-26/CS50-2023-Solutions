1a9c7c66e7f2012f7b1e6dce7910ddfeb73e1d29
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-05-01 12:46:33 +0000

    Mon, May 1, 2023, 8:46 PM +08:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,13 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- What tables does it have
+-- What tables does it have.
 .schema
 .tables
 
---Get more information about this table
+-- Get more information about this table.
 .schema crime_scene_reports
 
---Search for descriptions of crime on a particular day and particular street
+-- Find descriptions of the crime on a particular day and particular street.
 SELECT description
 FROM crime_scene_reports
 WHERE month = 7

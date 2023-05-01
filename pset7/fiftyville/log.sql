98505e72bf61a3895ed7d3f3c1a881573bceb592
BuHuiXiuDianNao <101173631+BuHuiXiuDianNao@users.noreply.github.com>
2023-05-01 12:50:17 +0000

    Mon, May 1, 2023, 8:50 PM +08:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -13,3 +13,14 @@ FROM crime_scene_reports
 WHERE month = 7
 AND day = 28
 AND street = 'Humphrey Street';
+
+-- Get more information about this table.
+.schema interviews
+
+-- View the interview records of three witnesses
+SELECT *
+FROM interviews
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND transcript LIKE "%thief%";
